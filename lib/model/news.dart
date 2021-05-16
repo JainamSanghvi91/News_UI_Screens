e7ab654bdd4418344a557feedf68889c32e063d7
Jainam Sanghvi <jainam1224@gmail.com>
2021-05-17 01:21:15 +0530

    Updated code

--- lib/model/news.dart
+++ lib/model/news.dart
@@ -1,8 +1,13 @@
-class News{
-  final String title;
-  final String url;
+class News {
+  final String headline;
+  final String image;
   final String date;
   final String description;
 
-  const News({this.title,this.date,this.description,this.url});
+  const News({
+    this.headline,
+    this.date,
+    this.description,
+    this.image,
+  });
 }
